32c4d54978ec838cc46fb7d27035ee89ee963eeb
Fernanda Mendes Naser <fernandamendesnaser@gmail.com>
2021-05-07 21:28:04 -0300

    Correção

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,21 +1,22 @@
 import Chart from 'react-apexcharts'
 
-const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-}
-
-const options = {
-    legend: {
-        show: true
+const DonutChart = () => {
+    const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-}
+
+    const options = {
+        legend: {
+            show: true
+        }
+    }
     return (
         <Chart
-        options={{...options, labels:mockData.labels}}
-        series={mockData.series}
-        type="donut"
-        height="240"
+            options={{ ...options, labels: mockData.labels }}
+            series={mockData.series}
+            type="donut"
+            height="240"
         />
     );
 }
